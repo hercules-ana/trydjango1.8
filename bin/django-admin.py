449d62bf60d291bd7c945a92da7b51e021371e02
Iraklis Anastasiou <helixzor@gmail.com>
2015-11-09 23:16:50 +0200

    first commit

--- bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/Anastasiou/Development/Websites/codingforentrepreneurs/trydjango18/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
